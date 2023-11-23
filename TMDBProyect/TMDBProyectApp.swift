ad3203fd07648891f5a8e27f3fb90ffa7aae3b8f
Carlos Lucas Sánchez <lukolukas@icloud.com>
2023-11-23 17:06:08 +0100

    Initial Commit

---
+++ TMDBProyect/TMDBProyectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBProyectApp.swift
+//  TMDBProyect
+//
+//  Created by Casa on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDBProyectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
